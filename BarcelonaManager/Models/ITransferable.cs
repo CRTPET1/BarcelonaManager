7cff741cef386d71cee623d68c480e05a5844bb3
Črt PETERLIN <crt.peterlin.21044@dijak.sc-celje.si>
2026-02-05 21:48:12 +0100

    Dodal razred ITransferable.cs

---
+++ BarcelonaManager/Models/ITransferable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcelonaManager.Models
+{
+    public interface ITransferable
+    {
+        bool CanBeTransferred();
+        void Transfer(decimal amount);
+    }
+}
